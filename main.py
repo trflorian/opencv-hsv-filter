b375422535e500269ae273755c7857c2c783d385
trflorian <florian.trautweiler@hispeed.ch>
2024-06-22 23:48:30 +0200

    cleanup

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 import cv2
 
 
-def get_channel_limit(channel_name):
+def get_channel_max(channel_name):
     return 180 if channel_name == "Hue" else 255
 
 
 class ImageFilter:
     def __init__(
         self,
-        img_path,
+        img_path: str,
         win_name="Image Filter",
         channels=["Hue", "Saturation", "Value"],
     ):
@@ -17,16 +17,16 @@ class ImageFilter:
 
         self.win_name = win_name
 
+        # initialize the channels and their max values
         self.channels = channels
-        self.channel_max_values = {c: get_channel_limit(c) for c in channels}
+        self.channel_max_values = {c: get_channel_max(c) for c in channels}
         self.init_filter_values()
 
+        # setup the gui
         cv2.namedWindow(self.win_name)
         self.create_trackbars()
 
-        self.show_filtered_image()
-
-    def init_filter_values(self):
+    def init_filter_values(self) -> None:
         """
         Initialize the filter values for each channel
         """
@@ -35,10 +35,11 @@ class ImageFilter:
             self.filter_values[f"{channel_name} Min"] = 0
             self.filter_values[f"{channel_name} Max"] = channel_max_value
 
-    def create_trackbars(self):
+    def create_trackbars(self) -> None:
         """
         Create trackbars for each channel and its min and max values
         """
+
         for channel_name, channel_max_value in self.channel_max_values.items():
             trackbar_name_min = f"{channel_name} Min"
             trackbar_name_max = f"{channel_name} Max"
@@ -48,7 +49,7 @@ class ImageFilter:
                 self.win_name,
                 0,
                 channel_max_value,
-                lambda value, name=trackbar_name_min: self.on_filter_value_changed(
+                lambda value, name=trackbar_name_min: self.on_trackbar_value_changed(
                     name, value
                 ),
             )
@@ -57,29 +58,70 @@ class ImageFilter:
                 self.win_name,
                 channel_max_value,
                 channel_max_value,
-                lambda value, name=trackbar_name_max: self.on_filter_value_changed(
+                lambda value, name=trackbar_name_max: self.on_trackbar_value_changed(
                     name, value
                 ),
             )
 
-    def on_filter_value_changed(self, trackbar_name, new_value):
+    def validate_trackbar_value(self, trackbar_name: str, new_value: int) -> int:
+        """
+        Validate the new value of the trackbar to be within the min and max values.
+        Adjust the trackbar value if it is out of bounds.
+
+        Args:
+            trackbar_name (str): the name of the trackbar
+            new_value (int): the new value of the trackbar
+
+        Returns:
+            int: the validated value of the trackbar
+        """
+        is_lower_bound = "Min" in trackbar_name
+
+        if is_lower_bound:
+            opposite_bound_name = trackbar_name.replace("Min", "Max")
+        else:
+            opposite_bound_name = trackbar_name.replace("Max", "Min")
+
+        opposite_bound_value = self.filter_values[opposite_bound_name]
+
+        if is_lower_bound and opposite_bound_value < new_value:
+            opposite_bound_value = new_value
+
+        if not is_lower_bound and opposite_bound_value > new_value:
+            opposite_bound_value = new_value
+
+        # update the trackbar value
+        cv2.setTrackbarPos(trackbar_name, self.win_name, new_value)
+        cv2.setTrackbarPos(opposite_bound_name, self.win_name, opposite_bound_value)
+
+        return new_value
+
+    def on_trackbar_value_changed(self, trackbar_name: str, new_value: int) -> None:
+        """
+        Callback function when a trackbar value is changed
+        """
+        new_value = self.validate_trackbar_value(trackbar_name, new_value)
         self.filter_values[trackbar_name] = new_value
+
         self.show_filtered_image()
 
-    def show_filtered_image(self):
-        lowerb = [
-            self.filter_values[f"{channel} Min"] for channel in self.channels
-        ]
-        upperb = [
-            self.filter_values[f"{channel} Max"] for channel in self.channels
-        ]
+    def show_filtered_image(self) -> None:
+        """
+        Show the filtered image based on the trackbar values
+        """
+        lowerb = [self.filter_values[f"{channel} Min"] for channel in self.channels]
+        upperb = [self.filter_values[f"{channel} Max"] for channel in self.channels]
 
         img_mask = cv2.inRange(self.img_hsv, tuple(lowerb), tuple(upperb))
         img_filtered = cv2.bitwise_and(self.img, self.img, mask=img_mask)
 
         cv2.imshow(self.win_name, img_filtered)
 
-    def run(self):
+    def run(self) -> None:
+        """
+        Run the image filter application
+        """
+        self.show_filtered_image()
         cv2.waitKey(0)
         cv2.destroyAllWindows()
 
