97697fa48ebeeac78a51626eb88a95a5e5497d7c
trflorian <florian.trautweiler@hispeed.ch>
2024-06-22 16:13:55 +0200

    using class for trackbar state management

--- main.py
+++ main.py
@@ -1,41 +1,69 @@
 import cv2
 
+img = cv2.imread("flower.png")
+
 win_name = "Image Filter"
 cv2.namedWindow(win_name)
 
-cv2.createTrackbar("Hue Min", win_name, 45, 180, lambda x: None)
-cv2.createTrackbar("Hue Max", win_name, 80, 180, lambda x: None)
 
-cv2.createTrackbar("Sat Min", win_name, 0, 255, lambda x: None)
-cv2.createTrackbar("Sat Max", win_name, 255, 255, lambda x: None)
+class ImageFilter:
+    def __init__(self, image, win_name) -> None:
+        self.image = image
+        self.image_hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+        self.win_name = win_name
 
-cv2.createTrackbar("Val Min", win_name, 0, 255, lambda x: None)
-cv2.createTrackbar("Val Max", win_name, 100, 255, lambda x: None)
+        self.hue_min, self.sat_min, self.val_min = 0, 0, 0
+        self.hue_max, self.sat_max, self.val_max = 180, 255, 255
+
+    def show_filtered_image(self):
+        img_mask = cv2.inRange(
+            self.image_hsv,
+            lowerb=(self.hue_min, self.sat_min, self.val_min),
+            upperb=(self.hue_max, self.sat_max, self.val_max),
+        )
+
+        img_filtered = cv2.bitwise_and(self.image, self.image, mask=img_mask)
+
+        cv2.imshow(self.win_name, img_filtered)
+
+    def on_hue_min_change(self, value):
+        self.hue_min = value
+        self.show_filtered_image()
+    
+    def on_hue_max_change(self, value):
+        self.hue_max = value
+        self.show_filtered_image()
+    
+    def on_sat_min_change(self, value):
+        self.sat_min = value
+        self.show_filtered_image()
+    
+    def on_sat_max_change(self, value):
+        self.sat_max = value
+        self.show_filtered_image()
+    
+    def on_val_min_change(self, value):
+        self.val_min = value
+        self.show_filtered_image()
+    
+    def on_val_max_change(self, value):
+        self.val_max = value
+        self.show_filtered_image()
 
 
-img = cv2.imread("flower.png")
+img_filter = ImageFilter(img, win_name)
 
-img_hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+cv2.createTrackbar("Hue Min", win_name, img_filter.hue_min, 180, img_filter.on_hue_min_change)
+cv2.createTrackbar("Hue Max", win_name, img_filter.hue_max, 180, img_filter.on_hue_max_change)
+
+cv2.createTrackbar("Sat Min", win_name, img_filter.sat_min, 255, img_filter.on_sat_min_change)
+cv2.createTrackbar("Sat Max", win_name, img_filter.sat_max, 255, img_filter.on_sat_max_change)
+
+cv2.createTrackbar("Val Min", win_name, img_filter.val_min, 255, img_filter.on_val_min_change)
+cv2.createTrackbar("Val Max", win_name, img_filter.val_max, 255, img_filter.on_val_max_change)
+
 
 while True:
-    hue_min = cv2.getTrackbarPos("Hue Min", win_name)
-    hue_max = cv2.getTrackbarPos("Hue Max", win_name)
-
-    sat_min = cv2.getTrackbarPos("Sat Min", win_name)
-    sat_max = cv2.getTrackbarPos("Sat Max", win_name)
-
-    val_min = cv2.getTrackbarPos("Val Min", win_name)
-    val_max = cv2.getTrackbarPos("Val Max", win_name)
-
-    img_mask = cv2.inRange(
-        img_hsv, lowerb=(hue_min, sat_min, val_min), upperb=(hue_max, sat_max, val_max)
-    )
-
-    img_filtered = cv2.bitwise_and(img, img, mask=img_mask)
-
-    cv2.imshow("Image Filtered", img_filtered)
-    cv2.imshow("Mask", img_mask)
-
     key = cv2.waitKey(1) & 0xFF
 
     if key == ord("q"):
